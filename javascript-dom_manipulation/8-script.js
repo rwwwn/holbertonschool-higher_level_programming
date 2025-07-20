68f4dd1b2d504b4e4c73aff66a5dd9fe3f7c1f2e
rwwwn <ralbaraiki@gmail.com>
2025-07-20 18:51:12 +0300

    Create 8-script.js

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const helloElement = document.getElementById('hello');
+
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      helloElement.textContent = data.hello;
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
+});
